f6a4b1914cb759647e463df3b00c1e3def3ee51f
Martin Owen <martinowenuk@gmail.com>
2016-03-17 20:18:07 +0000

    Implemented example 2.

---
+++ TypeScriptRedux/example02/HelloWorld.tsx
@@ -0,0 +1,7 @@
+﻿import * as React from 'react';
+
+export default class HelloWorld extends React.Component<any, any> {
+    render() {
+        return <div>Hello, World!</div>;
+    }
+}
---
+++ TypeScriptRedux/example02/app.js
@@ -0,0 +1,22 @@
+/// <reference path='../typings/browser.d.ts'/>
+System.register(['react', 'react-dom', './HelloWorld'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var React, ReactDOM, HelloWorld_1;
+    return {
+        setters:[
+            function (React_1) {
+                React = React_1;
+            },
+            function (ReactDOM_1) {
+                ReactDOM = ReactDOM_1;
+            },
+            function (HelloWorld_1_1) {
+                HelloWorld_1 = HelloWorld_1_1;
+            }],
+        execute: function() {
+            ReactDOM.render(React.createElement(HelloWorld_1.default, null), document.getElementById("content"));
+        }
+    }
+});
+//# sourceMappingURL=app.js.map
