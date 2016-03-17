8a47ee617051d63f48ca53d358193ace37e1d5ea
Martin Owen <martinowenuk@gmail.com>
2016-03-17 20:36:55 +0000

    Added Redux

---
+++ TypeScriptRedux/jspm_packages/npm/lodash-es@4.6.1/_baseCastFunction.js
@@ -0,0 +1,16 @@
+/* */ 
+"format cjs";
+import identity from './identity';
+
+/**
+ * Casts `value` to `identity` if it's not a function.
+ *
+ * @private
+ * @param {*} value The value to inspect.
+ * @returns {Array} Returns the array-like object.
+ */
+function baseCastFunction(value) {
+  return typeof value == 'function' ? value : identity;
+}
+
+export default baseCastFunction;
