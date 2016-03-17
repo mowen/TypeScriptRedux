8b271e6c5d6bf2c9cdd266d9bde73f56bdb859c3
Martin Owen <martinowenuk@gmail.com>
2016-03-17 20:12:53 +0000

    Moved dependencies to deps.tsx.

---
+++ TypeScriptRedux/deps.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from 'react';
+import { render } from 'react-dom';
+
+class Deps extends React.Component<any, any> {
+    render() {
+        return <div>Hello, World!</div>;
+    }
+}
+
+const ignore = () => render(<Deps/>, document.body);
