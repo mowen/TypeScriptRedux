ce571d4b9ca83f1f4dac51da72009c3d24ba5dd1
Martin Owen <martinowenuk@gmail.com>
2016-03-18 21:37:52 +0000

    Implemented example 7.

---
+++ TypeScriptRedux/example07/ActionPlayer.js
@@ -0,0 +1,57 @@
+System.register(['react'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+    var React;
+    var ActionPlayer;
+    return {
+        setters:[
+            function (React_1) {
+                React = React_1;
+            }],
+        execute: function() {
+            ActionPlayer = (function (_super) {
+                __extends(ActionPlayer, _super);
+                function ActionPlayer() {
+                    _super.apply(this, arguments);
+                }
+                ActionPlayer.prototype.componentDidMount = function () {
+                    var _this = this;
+                    this.unsubscribe = this.props.store.subscribe(function () { return _this.forceUpdate(); });
+                };
+                ActionPlayer.prototype.componentWillUnmount = function () {
+                    this.unsubscribe();
+                };
+                ActionPlayer.prototype.render = function () {
+                    var _this = this;
+                    return (React.createElement("div", null, React.createElement("button", {onClick: function (e) { return _this.replayActions(); }}, "replay"), React.createElement("p", null, React.createElement("b", null, this.props.actions.length), " actions"), React.createElement("button", {onClick: function (e) { return _this.undoAction(); }}, "Undo"), React.createElement("span", null), React.createElement("button", {onClick: function (e) { return _this.resetState(); }}, "Clear")));
+                };
+                ActionPlayer.prototype.resetState = function () {
+                    this.props.store.dispatch({ type: 'LOAD', state: this.props.defaultState });
+                    this.props.actions.length = 0;
+                };
+                ActionPlayer.prototype.replayActions = function () {
+                    var _this = this;
+                    var snapshot = this.props.actions.slice(0);
+                    this.resetState();
+                    snapshot.forEach(function (action, i) {
+                        return setTimeout(function () { return _this.props.store.dispatch(action); }, 10 * i);
+                    });
+                };
+                ActionPlayer.prototype.undoAction = function () {
+                    var _this = this;
+                    var snapshot = this.props.action.slice(0, this.props.actions.length - 1);
+                    this.resetState();
+                    snapshot.forEach(function (action) { return _this.props.store.dispatch(action); });
+                };
+                return ActionPlayer;
+            }(React.Component));
+            exports_1("default", ActionPlayer);
+        }
+    }
+});
+//# sourceMappingURL=ActionPlayer.js.map
--- TypeScriptRedux/example07/app.js
+++ TypeScriptRedux/example07/app.js
@@ -1,8 +1,14 @@
 /// <reference path='../typings/browser.d.ts'/>
-System.register(['react', 'react-dom', 'react-redux', './Counter'], function(exports_1, context_1) {
+System.register(["react", "react-dom", "redux", "react-redux", "./Counter", "./ShapeViewer", "./ColorPicker"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var React, ReactDOM, react_redux_1, Counter_1;
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+    var React, ReactDOM, redux_1, react_redux_1, Counter_1, ShapeViewer_1, ColorPicker_1;
+    var actions, defaultState, store, ColorWrapperBase, ColorWrapper;
     return {
         setters:[
             function (React_1) {
@@ -11,14 +17,60 @@ System.register(['react', 'react-dom', 'react-redux', './Counter'], function(exp
             function (ReactDOM_1) {
                 ReactDOM = ReactDOM_1;
             },
+            function (redux_1_1) {
+                redux_1 = redux_1_1;
+            },
             function (react_redux_1_1) {
                 react_redux_1 = react_redux_1_1;
             },
             function (Counter_1_1) {
                 Counter_1 = Counter_1_1;
+            },
+            function (ShapeViewer_1_1) {
+                ShapeViewer_1 = ShapeViewer_1_1;
+            },
+            function (ColorPicker_1_1) {
+                ColorPicker_1 = ColorPicker_1_1;
             }],
         execute: function() {
-            ReactDOM.render(React.createElement(react_redux_1.Provider, null, React.createElement(Counter_1.default, {field: "width", step: 10}), React.createElement(Counter_1.default, {field: "height", step: 10})), document.getElementById('content'));
+            actions = [];
+            defaultState = { nextShapeId: 0, width: 100, height: 100, color: "#000000", shapes: [] };
+            store = redux_1.createStore(function (state, action) {
+                actions.push(action);
+                switch (action.type) {
+                    case "COUNTER_CHANGE":
+                        return Object.assign({}, state, (_a = {}, _a[action.field] = state[action.field] + action.by, _a));
+                    case "COLOR_CHANGE":
+                        return Object.assign({}, state, { color: action.color });
+                    case "SHAPE_ADD":
+                        var id = state.nextShapeId;
+                        var shape = Object.assign({}, { id: id }, action);
+                        delete shape['type'];
+                        return Object.assign({}, state, { nextShapeId: id + 1, shapes: state.shapes.concat([shape]) });
+                    case "SHAPE_CHANGE":
+                        var shape = Object.assign({}, state.shapes.filter(function (x) { return x.id === action.id; })[0], { top: action.top, left: action.left });
+                        return Object.assign({}, state, { shapes: state.shapes.filter(function (x) { return x.id !== action.id; }).concat([shape]) });
+                    case "LOAD":
+                        return action.state;
+                    default:
+                        return state;
+                }
+                var _a;
+            }, defaultState);
+            ColorWrapperBase = (function (_super) {
+                __extends(ColorWrapperBase, _super);
+                function ColorWrapperBase() {
+                    _super.apply(this, arguments);
+                }
+                ColorWrapperBase.prototype.render = function () {
+                    return React.createElement(ColorPicker_1.ColorPicker, {color: this.props.color, onChange: this.props.setColor});
+                };
+                return ColorWrapperBase;
+            }(React.Component));
+            ColorWrapper = react_redux_1.connect(function (state) { return ({ color: state.color }); }, function (dispatch) { return ({
+                setColor: function (color) { return dispatch({ type: "COLOR_CHANGE", color: color }); }
+            }); })(ColorWrapperBase);
+            ReactDOM.render(React.createElement(react_redux_1.Provider, {store: store}, React.createElement("table", null, React.createElement("tbody", null, React.createElement("tr", null, React.createElement("td", {style: { width: 220 }}, React.createElement(Counter_1.default, {field: "width", step: 10}), React.createElement(Counter_1.default, {field: "height", step: 10}))))), React.createElement(ShapeViewer_1.default, null)), document.getElementById('content'));
         }
     }
 });
